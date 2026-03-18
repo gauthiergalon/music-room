631fd135a3c7085fde14714d7b588f07b57adb4e
gogolescargot <gauthiergalon@pm.me>
2026-03-18 20:17:16 +0100

    Add cleanup

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -24,9 +24,11 @@ pub async fn run() {
 
 	let cors = CorsLayer::new().allow_methods([Method::GET, Method::POST, Method::PUT, Method::DELETE]).allow_headers(Any).allow_origin(Any);
 
-	let app = routes::app_router().layer(cors).layer(TraceLayer::new_for_http()).with_state(pool);
+	let app = routes::app_router().layer(cors).layer(TraceLayer::new_for_http()).with_state(pool.clone());
 
 	let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+
+	services::cleanup::spawn_token_cleanup_task(pool);
 
 	tracing::info!("Backend running on port 3000");
 	axum::serve(listener, app).await.unwrap();
---
+++ backend/src/services/cleanup.rs
@@ -0,0 +1,26 @@
+use chrono::Utc;
+use sqlx::PgPool;
+use std::time::Duration;
+use tokio::time;
+
+pub fn spawn_token_cleanup_task(pool: PgPool) {
+	tokio::spawn(async move {
+		let mut interval = time::interval(Duration::from_secs(60 * 60 * 24));
+
+		loop {
+			interval.tick().await;
+
+			let res_refresh = sqlx::query!("DELETE FROM refresh_tokens WHERE expires_at < $1", Utc::now()).execute(&pool).await;
+
+			if let Ok(result) = res_refresh {
+				println!("Cleaned up {} expired refresh tokens", result.rows_affected());
+			}
+
+			let res_reset = sqlx::query!("DELETE FROM reset_tokens WHERE expires_at < $1", Utc::now()).execute(&pool).await;
+
+			if let Ok(result) = res_reset {
+				println!("Cleaned up {} expired reset tokens", result.rows_affected());
+			}
+		}
+	});
+}
