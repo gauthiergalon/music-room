d96069d6060bb13950d3af5ff67fe87a4126952f
gauthiergalon <gauthiergalon@pm.me>
2026-05-12 18:15:43 +0200

    chore(backend): add NOT NULL constraints to users table
    - email_confirmed: BOOLEAN NOT NULL DEFAULT FALSE
    - favorite_genres: TEXT[] NOT NULL DEFAULT '{}'
    
    These columns should always have a value.

--- backend/migrations/0_users.sql
+++ backend/migrations/0_users.sql
@@ -4,10 +4,10 @@ CREATE TABLE users (
     id              UUID DEFAULT gen_random_uuid() PRIMARY KEY,
     username        VARCHAR(24) UNIQUE NOT NULL,
     email           VARCHAR(255) UNIQUE NOT NULL,
-    email_confirmed BOOLEAN DEFAULT FALSE,
+    email_confirmed BOOLEAN NOT NULL DEFAULT FALSE,
     password_hash   TEXT,
     google_id       VARCHAR(255) UNIQUE,
-    favorite_genres TEXT[] DEFAULT '{}',
+    favorite_genres TEXT[] NOT NULL DEFAULT '{}',
     privacy_level   privacy_level NOT NULL DEFAULT 'friends'
 );
 
